fa62b923a05b3e1eac3bbef901a47b25f2013438
shanxu <kingundertree@163.com>
2015-04-01 11:35:42 +0800

    add extensionNavGesForiOS7

---
+++ ExtensionNavGesForiOS7/ExtensionNav.m
@@ -0,0 +1,66 @@
+//
+//  ExtensionNav.m
+//  ExtensionNavGesForiOS7
+//
+//  Created by xiazer on 15/4/1.
+//  Copyright (c) 2015年 anjuke. All rights reserved.
+//
+
+#import "ExtensionNav.h"
+
+// 来自 http://www.jianshu.com/p/d39f7d22db6c
+
+@interface ExtensionNav ()
+
+@end
+
+@implementation ExtensionNav
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    UIGestureRecognizer *gesture = self.interactivePopGestureRecognizer;
+    gesture.enabled = NO;
+    UIView *gestureView = gesture.view;
+    
+    UIPanGestureRecognizer *popRecognizer = [[UIPanGestureRecognizer alloc] init];
+    popRecognizer.delegate = self;
+    popRecognizer.maximumNumberOfTouches = 1;
+    [gestureView addGestureRecognizer:popRecognizer];
+
+    /**
+     *  获取系统手势的target数组
+     */
+    NSMutableArray *_targets = [gesture valueForKey:@"_targets"];
+    /**
+     *  获取它的唯一对象，我们知道它是一个叫UIGestureRecognizerTarget的私有类，它有一个属性叫_target
+     */
+    id gestureRecognizerTarget = [_targets firstObject];
+    /**
+     *  获取_target:_UINavigationInteractiveTransition，它有一个方法叫handleNavigationTransition:
+     */
+    id navigationInteractiveTransition = [gestureRecognizerTarget valueForKey:@"_target"];
+    /**
+     *  通过前面的打印，我们从控制台获取出来它的方法签名。
+     */
+    SEL handleTransition = NSSelectorFromString(@"handleNavigationTransition:");
+    /**
+     *  创建一个与系统一模一样的手势，我们只把它的类改为UIPanGestureRecognizer
+     */
+    [popRecognizer addTarget:navigationInteractiveTransition action:handleTransition];
+}
+
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    /**
+     *  这里有两个条件不允许手势执行，1、当前控制器为根控制器；2、如果这个push、pop动画正在执行（私有属性）
+     */
+    return self.viewControllers.count != 1 && ![[self valueForKey:@"_isTransitioning"] boolValue];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
